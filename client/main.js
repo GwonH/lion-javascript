a479d07901237e35e13e2254dcbabd5028f48bed
GwonH <kk.filament@gmail.com>
2023-07-31 09:14:49 +0900

    07.storage 학습

--- client/main.js
+++ client/main.js
@@ -1,3 +1,32 @@
-console.log('hello js!');
 
-const a = 10;
+import { getNode, getStorage, setStorage } from "./lib/index.js";
+
+const textField = getNode('#textField')
+const clearButton = getNode('button')
+
+function handleTextField(){
+  const value = this.value;
+
+  setStorage('text',value)
+
+
+}
+
+function init(){
+
+  getStorage('text').then((res)=>{
+    textField.value=res;
+  })
+
+}
+
+function handleClear(){
+
+
+
+}
+
+
+textField.addEventListener('input',handleTextField)
+window.addEventListener('DOMContentLoaded',init)
+clearButton.addEventListener('click',handleClear)
